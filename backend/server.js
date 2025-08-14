713a6fcc8fc26fe6d3a444005c588a84d69fdacd
Mohan <mohan@k12savings.com>
2025-08-14 15:41:27 -0500

    made some changes on env file

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+app.use('/api/auth', authRoutes);
 
+
+
+
+
+
+
+
+//MongoDB Connection
+
+const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -28,4 +38,4 @@ mongoose.connect(process.env.MONGO_URI, {
 
 
 
-app.use('/api/auth', authRoutes);
+
